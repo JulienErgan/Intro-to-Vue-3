2a2d8c456c12b3f1e416dc3f43f69dbf0868fafa
Julien Ergan <julien.ergan.code@gmail.com>
2021-07-19 11:12:22 +0900

    Created App with and without ES6 shorthand

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const product = 'Socks'
+// const product = 'Socks'
+
+// without ES6 shorthand
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks'
+        }
+    }
+})
+
+// with ES6 shorthand
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks'
+        }
+    }
+})
