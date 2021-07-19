39fe24c2beb54e9169c80c3f03a378f43bf251a3
Julien Ergan <julien.ergan.code@gmail.com>
2021-07-19 13:23:45 +0900

    Added onSale condition

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             // inStock: false
-            inventory: 8
+            inventory: 8,
+            onSale: true
         }
     }
 })
